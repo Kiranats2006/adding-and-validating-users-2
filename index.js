ee5bd17b05338cd3bd78eec431824cc49e780fc6
Kirana Trupthi Shree <kiranats2006@gmail.com>
2025-02-06 09:31:16 +0530

    DOne

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require('express');
-const { resolve } = require('path');
-
+const bcrypt = require('bcryptjs');
 const app = express();
-const port = 3010;
+const port = 3000;
 
-app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+const usersDB = [
+    { email: 'user@example.com', password: '$2b$10$examplehashedpassword' }
+];
+
+app.use(express.json());
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Email and password are required' });
+    }
+
+    const user = usersDB.find((user) => user.email === email);
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+
+   
+    try {
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (isPasswordValid) {
+            res.status(200).json({ message: 'Login successful' });
+        } else {
+            res.status(401).json({ message: 'Invalid credentials' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
